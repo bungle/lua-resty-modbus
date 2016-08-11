dcd4a6ec0ad834d581d1bb202920dc8655e41796
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2016-08-11 23:32:37 +0300

    More testing.

--- lib/test.lua
+++ lib/test.lua
@@ -11,10 +11,8 @@ rtu:set_debug(true)
 
 print("Connecting", rtu:connect())
 
-print("Setting slave", rtu:set_slave(1))
-
 print("Getting serial mode", rtu:get_serial_mode())
-print("Setting serial mode", rtu:set_serial_mode("rs485"))
+print("Setting serial mode", rtu:set_serial_mode("rs232"))
 print("Getting serial mode", rtu:get_serial_mode())
 
 print("Getting RTS mode", rtu:get_rts())
@@ -24,7 +22,6 @@ print("Getting RTS mode", rtu:get_rts())
 print("Getting RTS delay", rtu:get_rts_delay())
 print("Setting RTS delay", rtu:set_rts_delay(86))
 print("Getting RTS delay", rtu:get_rts_delay())
-
 
 print("Getting byte timeout", rtu:get_byte_timeout())
 print("Setting byte timeout", rtu:set_byte_timeout(0, 500000))
@@ -36,14 +33,24 @@ print("Getting response timeout", rtu:get_response_timeout())
 
 print("Getting header length", rtu:get_header_length())
 
+print("Setting slave", rtu:set_slave(1))
+
 print("Sending Raw Request", rtu:send_raw_request("\x01\x2b\x0e\x01\x00\x70\x77"))
 print("Receiving Confirmation", rtu:receive_confirmation())
 
-print("Reading bits", rtu:read_bits(0x3101, 1))
-print("Reading input bits", rtu:read_input_bits(0x3101, 1))
+print("Reading bits", rtu:read_bits(0x3, 1))
+print("Reading input bits", rtu:read_input_bits(0x3, 1))
 
-print("Reading registers", rtu:read_registers(0x3101, 1))
-print("Reading input registers", rtu:read_input_registers(0x3101, 1))
+print("Reading registers", rtu:read_registers(0x3, 1))
+print("Reading input registers", rtu:read_input_registers(0x3, 1))
 
+print("Reading registers", rtu:read_registers(0x3000, 1))
+print("Reading input registers", rtu:read_input_registers(0x3000, 1))
+
+print("Reading registers", rtu:read_registers(3000, 1))
+print("Reading input registers", rtu:read_input_registers(3000, 1))
+
+print("Reading registers", rtu:read_registers(12288, 1))
+print("Reading input registers", rtu:read_input_registers(12288, 1))
 
 rtu:close()
